86d9ec74f788a42f8b29be7175e0952cc171ec2f
Sergio Navarro <sergio.navarro@vision33.co.uk>
2019-04-10 11:45:37 +0100

    Finished array course section

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -17,11 +17,20 @@ namespace Acme.Biz
         public Product()
         {
             string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
+
+            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
+
+            colorOptions.SetValue("Blue", 3);
+
+            for (int i = 0; i < colorOptions.Length; i++)
+            {
+                colorOptions[i] = colorOptions[i].ToLower();
+            }
+
             foreach (var color in colorOptions)
             {
                 Console.WriteLine($"The color is {color}");
             }
-
         }
         public Product(int productId,
                         string productName,
